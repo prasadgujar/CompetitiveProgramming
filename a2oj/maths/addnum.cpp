b0ec57a58d8b7dba66a745b7918a2839fdd7dc6a
prasad <prasadgujar16@gmail.com>
2018-05-06 02:19:17 +0530

    a2oj

---
+++ a2oj/maths/addnum.cpp
@@ -0,0 +1,21 @@
+
+ #include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void solve()
+{
+    ll a,b;
+    cin>>a>>b;
+    
+
+    cout<<a+b<<'\n';
+}
+int main()
+{
+ int t;
+ cin>>t;
+ while(t--)
+ {
+     solve();
+ }
+}
