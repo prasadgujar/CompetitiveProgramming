bde1102f6f69670ad12e1af01c11aed70bd20f9a
Prasad Gujar <prasadgujar16@gmail.com>
2018-08-26 15:17:56 +0530

    bc+

--- Hackerearth/bc/b.cpp
+++ Hackerearth/bc/b.cpp
@@ -1,96 +1,165 @@
-#include <iostream>
-#include <cstdlib>
-
+#include<bits/stdc++.h>
 using namespace std;
-#define MAX_MATCHES 200000000
 
-//Array to store matched indexes
-long long int FOUND[MAX_MATCHES];
-long long int FOUND[MAX_MATCH];
-//variable to store last index in FOUND array
-static long long int l = 0;
+void getZarr(string str, int Z[]);
 
-//Partial match table
-void kmp_table(string W, int *T )
+vector<long long int> v1;
+vector<long long int> v2;
+
+void  search(string text, string pattern)
 {
- long long int pos = 2;
- long long int cnd = 0;
- long long int length = W.length();
- 
- T[0] = -1;
- T[1] = 0;
- 
- while( pos < length)
- {
-  if(W[pos-1] == W[cnd])
-  {
-   T[pos] = cnd + 1;
-   cnd++;
-   pos++;
-  }
-  else if( cnd > 0)
-   cnd = T[cnd];
-  else
-  {
-   T[pos] = 0;
-   pos++;
-  }
- }
+	
+	string concat = pattern + "$" + text;
+	int l = concat.length();
+
+	int Z[l];
+	getZarr(concat, Z);
+
+	for (int i = 0; i < l; ++i)
+	{
+	
+		if (Z[i] == pattern.length())
+			{
+				//cout << "Pattern found at index "
+				//<< i - pattern.length() -1 << endl;
+			
+				v1.push_back(i - pattern.length() -1);
+			
+			}
+	}
+	
 }
 
-//Search function
-void kmp_search(string S, string W)
+void  searchs(string text, string pattern)
 {
- 
- int m = 0; 
- int i = 0;
- int sizeS = S.length();
- int sizeW = W.length();
- 
- int *T = new int[sizeof(int) * sizeW];
- 
- kmp_table(W, T);
- 
- while( (m + i) < sizeS)
- {
-  if (W[i] == S[m + i]) 
-  {
-            if (i == (sizeW - 1))
-            {
-             
-             FOUND[l++] = m;
-   }
-    
-            i++;
-        }
-        else
-        {
-            if (T[i] > -1)
-            {
-                m = m + i - T[i];
-    i = T[i];
-            }
-            else
-            {
-                m = m + 1;
-    i = 0;
-            }
-        }
- }
- 
- delete(T);
+	
+	string concat = pattern + "$" + text;
+	int l = concat.length();
+
+	int Z[l];
+	getZarr(concat, Z);
+
+	for (int i = 0; i < l; ++i)
+	{
+	
+		if (Z[i] == pattern.length())
+			{
+				//cout << "Pattern found at index "
+				//<< i - pattern.length() -1 << endl;
+			
+				v2.push_back(i - pattern.length() -1);
+			
+			}
+	}
+	
+}
+void getZarr(string str, int Z[])
+{
+	int n = str.length();
+	int L, R, k;
+
+
+	L = R = 0;
+	for (int i = 1; i < n; ++i)
+	{
+	
+		if (i > R)
+		{
+			L = R = i;
+			while (R<n && str[R-L] == str[R])
+				R++;
+			Z[i] = R-L;
+			R--;
+		}
+		else
+		{
+		
+			k = i-L;
+			if (Z[k] < R-i+1)
+				Z[i] = Z[k];
+			else
+			{
+				
+				L = i;
+				while (R<n && str[R-L] == str[R])
+					R++;
+				Z[i] = R-L;
+				R--;
+			}
+		}
+	}
 }
 
 int main()
 {
- string s;
- sting W;
- string b;
- 
- 
- kmp_search(S,W);
-  
- for (long long int i = 0 ; i < l; i++)
-  cout<<"Pattern found at "<< FOUND[i] <<endl; 
-  
+	string a,b,s;
+	cin>>s;
+	cin>>a;
+	cin>>b;
+	search(s, a);
+	
+	searchs(s,b);
+	/*for(int i=0;i<v1.size();i++)
+	{
+		cout<<v1[i]<<"\n";
+	}
+		for(int i=0;i<v2.size();i++)
+	{
+		cout<<v2[i]<<"\n";
+	}*/
+	long long int na = 2000000001;
+	long int x,y;
+	int c1;
+	for(int i=0;i<v1.size();i++)
+	{
+		for(int j=0;j<v2.size();j++)
+		{
+			long int ok  = abs(v1[i]-v2[j]);
+			//cout<<ok<<" ";
+			//minn = min(minn,ok);
+			if(ok<=na)
+			{
+				na = ok;
+				x = min(v1[i],v2[j]);
+				y = max(v1[i],v2[j]);
+			            if(v1[i]>v2[j])
+			            {
+			            	c1  = 1;
+			            }
+			            else if(v2[j]<v1[i])
+			            {
+			            	c1 = 2;
+			            }
+			}
+		}
+	}
+	//cout<<na<<" "<<x<<" "<<y;
+	long int al = a.length();
+	long int bl = b.length();
+	long long  int sl = s.length();
+	long long int res = 0; 
+	long long int pl = 0;
+	if(c1==1)
+	{
+		pl = a.length();
+	}
+	else
+	{
+		pl = b.length();
+	}
+	x++;
+	y++;
+	//cout<<sl;
+	//cout<<a.length();
+	long int cc = max(x,y);
+	long long int sub = sl-cc;
+	//cout<<sub;
+	res  = x*(sub-pl);
+	if(res<0)
+	{
+		res = res * - 1;
+	}
+	cout<<res+1;
+	//cout<<x;
+	return 0;
 }
